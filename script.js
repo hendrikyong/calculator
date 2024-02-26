4c0e1513a003d8cf4b640a8322e5e12a6f713246
Hendrik Yong <hendrikyong1205@gmail.com>
2024-02-26 15:09:23 +0800

    calculation function in

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttonsContainer = document.querySelector(".buttons");
+  let displayValue = "";
 
-  // Create divs
   for (let i = 1; i < 6; i++) {
     const div = document.createElement("div");
     div.classList.add(`buttons-row${i}`);
     buttonsContainer.appendChild(div);
   }
 
-  // Items to add to each div
   const items = [
     ["clear", "%", "√", "/"], // Items for buttons-row1
     ["7", "8", "9", "*"], // Items for buttons-row2
@@ -17,13 +16,28 @@ document.addEventListener("DOMContentLoaded", function () {
     ["0", ".", "="], // Items for buttons-row5
   ];
 
-  // Iterate over each div and add items
   items.forEach((item, index) => {
     const div = document.querySelector(`.buttons-row${index + 1}`);
     item.forEach((text) => {
       const button = document.createElement("button");
       button.textContent = text;
       button.classList.add("btn", "btn-secondary", "m-1");
+      button.addEventListener("click", () => {
+        if (text === "clear") {
+          displayValue = "";
+        } else if (text === "=") {
+          displayValue = eval(displayValue);
+          displayValue = Math.round(displayValue * 1e10) / 1e10;
+        } else if (text === "√") {
+          displayValue = Math.sqrt(Number(displayValue));
+        } else if (text === "%") {
+          displayValue = Number(displayValue) / 100;
+        } else {
+          displayValue += text;
+        }
+        console.log(`${text} clicked`);
+        document.getElementById("display").innerHTML = displayValue;
+      });
       div.appendChild(button);
     });
   });
