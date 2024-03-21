ede74d7dd2f903478bb76b0ecce171223e5aa16b
Hendrik Yong <hendrikyong1205@gmail.com>
2024-03-21 17:05:25 +0800

    update again

--- script.js
+++ script.js
@@ -1,48 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttonsContainer = document.querySelector(".buttons");
+  const display = document.getElementById("display");
   let equation = "";
   let result = "";
-  const display = document.getElementById("display");
 
-  //create rows
-  for (let i = 1; i < 6; i++) {
-    const div = document.createElement("div");
-    div.classList.add(`buttons-row${i}`);
-    buttonsContainer.appendChild(div);
-  }
-
-  const items = [
-    ["clear", "%", "√", "/"],
-    ["7", "8", "9", "*"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", ".", "="],
-  ];
-
-  items.forEach((item, index) => {
-    const div = document.querySelector(`.buttons-row${index + 1}`);
-    item.forEach((text) => {
-      //create button
-      const button = document.createElement("button");
-      button.textContent = text;
-      button.classList.add("btn", "btn-secondary", "m-1");
-      button.addEventListener("click", () => {
-        // console.log("text", text); //logs each click
-        if (text === "clear") {
-          clear();
-        } else if (text === "=") {
-          //here needs to evaluate
-          evaluate();
-        } else {
-          equation += text;
-        }
-        display.innerHTML =
-          equation + "<br>" + "<span class='result'>" + result + "</span>";
-      });
-      div.appendChild(button);
-    });
-  });
-
+  //clear function
   function clear() {
     equation = "";
     result = "";
@@ -113,31 +75,45 @@ document.addEventListener("DOMContentLoaded", function () {
         ? result
         : parseFloat(result).toFixed(5);
     }
-    //--------------CODE ABOVE IS GOOD DO NOT DELETE THIS COMMENT UNTIL DONE---------------
-    //adding code to help further calculation rather than reevaluating the whole line
-    //.buttons-row5 button:nth-child(3) targets = button
-    //have to calculate further by storing result in new eqn and then eval new eqn and see whats up
-    let addOp = "";
-    let modifiedEqn = "";
-    let newResult = eval(modifiedEqn);
-    document.querySelectorAll(".buttons button").forEach((button) => {
-      button.addEventListener("click", function () {
-        console.log("text con", this.textContent);
-        if (this.textContent !== "=") {
-          addOp += this.textContent;
-          console.log("add op", addOp);
-          modifiedEqn = result + addOp;
-          console.log("mod eqn", modifiedEqn);
-          newResult = eval(modifiedEqn); //i did get 13.5 on console.log but this line error
-          console.log("new res", newResult);
-          result = newResult;
-          display.innerHTML =
-            equation + "<br>" + "<span class='result'>" + newResult + "</span>"; // Update the display
-        }
-      });
-    });
-
-    //this one is for when = clicked and any operations aft that then calculate further rather than reevaluate the
-    //whole eqn again
   }
+
+  //create buttons row and append them to buttonsContainter
+  for (let i = 1; i < 6; i++) {
+    const div = document.createElement("div");
+    div.classList.add(`buttons-row${i}`);
+    buttonsContainer.appendChild(div);
+  }
+
+  //buttons
+  const items = [
+    ["clear", "%", "√", "/"],
+    ["7", "8", "9", "*"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="],
+  ];
+
+  items.forEach((item, index) => {
+    const div = document.querySelector(`.buttons-row${index + 1}`);
+    item.forEach((text) => {
+      //create button
+      const button = document.createElement("button");
+      button.textContent = text;
+      button.classList.add("btn", "btn-secondary", "m-1");
+      button.addEventListener("click", () => {
+        console.log("text", text); //logs each click
+        if (text === "clear") {
+          clear();
+        } else if (text === "=") {
+          //here needs to evaluate
+          evaluate();
+        } else {
+          equation += text;
+        }
+        display.innerHTML =
+          equation + "<br>" + "<span class='result'>" + result + "</span>";
+      });
+      div.appendChild(button);
+    });
+  });
 });
