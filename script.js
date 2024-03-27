b263a4556d9628184b0ed71f00ad10f508ee2eba
Hendrik Yong <hendrikyong1205@gmail.com>
2024-03-27 23:38:01 +0800

    still does not handle further operations

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
   //declare variables
   let equation = "";
   let result = "";
-  let isEqualsPressed = false; //to track if equals has been pressed if yes then
-  //need to use further calc with old res + new operations to derive new result
-  let newEqn = ""; //for when there is further calculations (not to store the neweqn its to help to derive the new res)
-  let newResult = ""; //to track the new result when there is further calculations
-  let addOp = ""; //if there is additional operations to craft new eqn
 
   function clear() {
     equation = "";
@@ -54,12 +49,14 @@ document.addEventListener("DOMContentLoaded", function () {
       result = Math.round(result * 100) / 100;
       console.log(result);
     } else {
-      //now that i have checked for sqrt and pct now is to evaluate those eqn without
-      //any special characters in them
-      //but i still need to check that if equals have been pressed
-      //use newEqn to store prev res + add ops then eval and store res in new res
-      //temp
       result = eval(equation);
+    }
+    if (!isFinite(result)) {
+      result = "Math Error";
+    } else {
+      result = Number.isInteger(parseFloat(result))
+        ? result
+        : parseFloat(result).toFixed(5);
     }
   }
 
@@ -87,12 +84,9 @@ document.addEventListener("DOMContentLoaded", function () {
       button.textContent = text;
       button.classList.add("btn", "btn-secondary", "m-1");
       button.addEventListener("click", () => {
-        //logs button clicks
-        //console.log(button.textContent);
         if (text === "clear") {
           clear();
         } else if (text === "=") {
-          //if the button clicked is = i need to evaluate the equation
           evaluate();
         } else {
           equation += text;
@@ -103,38 +97,4 @@ document.addEventListener("DOMContentLoaded", function () {
       div.appendChild(button);
     });
   });
-
-  //test case 5*5+2 = 27 /2 = 13.5
-  const equals = document.querySelector(".buttons-row5 button:nth-child(3)");
-  if (equals) {
-    equals.addEventListener("click", function () {
-      console.log("first = click");
-      isEqualsPressed = true;
-      if (result >= 0) {
-        //that means that equals has been hit and there was a prev operation already calculated
-        //find a way to track add op aft that add that shit to new eqn once done then evaluate
-        //turn this whole thing into a function and then use if else at the else block of evaluate
-        document.querySelectorAll(".buttons button").forEach((button) => {
-          button.addEventListener("click", function () {
-            if (this.textContent !== "=") {
-              //then add it to add op
-              //after that craft newEqn and then eval
-              addOp += this.textContent;
-              console.log("add op", addOp);
-              newEqn = result + addOp;
-            }
-            console.log("res", result);
-            console.log(equation);
-            console.log("new eqn", newEqn);
-            //newResult = eval("8+2");
-            //new eqn got some issue
-            newResult = eval(newEqn);
-            console.log("new res", newResult);
-            display.innerHTML =
-              newEqn + "<br>" + "<span class='result'>" + newResult + "</span>";
-          });
-        });
-      }
-    });
-  }
 });
