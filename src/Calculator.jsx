f2e8a5893cbd6099a85b5582e00c9e3aabc78b94
Hendrik Yong <hendrikyong1205@gmail.com>
2024-08-27 21:48:03 +0800

    included calculate use state and installed mathjs

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,38 +1,67 @@
 import Button from "./Button";
 import styles from "../src/css/calculator.module.css";
+import React, { useState } from "react";
+import { evaluate } from "mathjs";
+
 function Calculator() {
-  const handleButtonClick = (label) => {
-    console.log(`Button clicked: ${label}`);
-  };
+  const [equation, setEquation] = useState("");
+  const [answer, setAnswer] = useState();
+  const [calculated, setCalculated] = useState(false);
+
+  function handleButtonClick(label) {
+    setEquation((e) => e + label);
+  }
+
+  function handleArithmetic(label) {
+    setEquation((e) => e + label);
+  }
+
+  function handleCalculate() {
+    console.log("Calculate button clicked");
+    try {
+      // i want to say check if answer has a value if there is any operations after the equals have been hit must be calculated with that value and not the equation
+      setAnswer(evaluate(equation));
+    } catch (e) {
+      setAnswer("Error");
+    }
+  }
+
+  function handleClear() {
+    setEquation("");
+    setAnswer("");
+  }
   return (
     <>
       <div className={styles.calcCover}>
         <div className={styles.calcContainer}>
-          <div className={styles.calcScreen}></div>
+          <div className={styles.calcScreen}>
+            <div className={styles.eqn}>{equation}</div>
+            <div className={styles.ans}>{answer}</div>
+          </div>
           <div className={styles.calcButtons}>
-            <Button label="AC" onClick={handleButtonClick} />
-            <Button label="√" onClick={handleButtonClick} />
-            <Button label="%" onClick={handleButtonClick} />
-            <Button label="/" onClick={handleButtonClick} />
+            <Button label="AC" onClick={handleClear} />
+            <Button label="√" onClick={handleArithmetic} />
+            <Button label="%" onClick={handleArithmetic} />
+            <Button label="/" onClick={handleArithmetic} />
             <Button label="7" onClick={handleButtonClick} />
             <Button label="8" onClick={handleButtonClick} />
             <Button label="9" onClick={handleButtonClick} />
-            <Button label="*" onClick={handleButtonClick} />
+            <Button label="*" onClick={handleArithmetic} />
             <Button label="4" onClick={handleButtonClick} />
             <Button label="5" onClick={handleButtonClick} />
             <Button label="6" onClick={handleButtonClick} />
-            <Button label="-" onClick={handleButtonClick} />
+            <Button label="-" onClick={handleArithmetic} />
             <Button label="1" onClick={handleButtonClick} />
             <Button label="2" onClick={handleButtonClick} />
             <Button label="3" onClick={handleButtonClick} />
-            <Button label="+" onClick={handleButtonClick} />
+            <Button label="+" onClick={handleArithmetic} />
             <Button
               label="0"
               onClick={handleButtonClick}
               className={styles.wideButton}
             />
             <Button label="." onClick={handleButtonClick} />
-            <Button label="=" onClick={handleButtonClick} />
+            <Button label="=" onClick={handleCalculate} />
           </div>
         </div>
       </div>
